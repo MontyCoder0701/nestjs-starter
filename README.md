0b2b849e6eeb14e019e5359d69bd605294248329
Soojeong <soojlee0106@naver.com>
2025-07-28 13:20:43 +0900

    feat: setup basic template for nestjs
    Update README.md
    
    build: setup typeorm and mysql
    
    build: setup env
    
    Update README.md
    
    build: setup workflows

---
+++ README.md
@@ -0,0 +1,34 @@
+# Nest.js Starter
+
+This is a simple starter repository for Nest.js projects.  
+Use it for hackathons or external projects as a boilerplate — clone and customize as needed.
+
+## What's Included
+
+### GitHub Actions
+
+- **Release Please** – automated release management
+- **Dependabot** – automatic dependency updates
+- **Dependabot Validation** - automatic build check for dependabot PRs
+- **CI** – basic continuous integration for build, tests, lint checks
+
+### Database
+
+- **MySQL**
+- **TypeORM**
+
+## Notice
+
+If you're using Dependabot, ensure the following setting is enabled:
+
+> **Settings → Actions → General → Allow GitHub Actions to create and approve pull requests**
+
+Add `.env.development` and `.env.production` to root directory to run. Fill in appropriate values for each.
+
+```md
+DATABASE_HOST=
+DATABASE_PORT=
+DATABASE_USERNAME=
+DATABASE_PASSWORD=
+DATABASE_NAME=
+```
---
+++ src/app.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath:
+        process.env.NODE_ENV === 'production'
+          ? '.env.production'
+          : '.env.development',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        autoLoadEntities: true,
+        // WARN: only for development
+        // synchronize: true,
+      }),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
